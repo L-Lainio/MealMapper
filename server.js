05b42c9cabc6b289935fdca69cd934cc488853bb
Hunter <hunterde91@gmail.com>
2024-05-16 10:32:50 -0400

    worked on adding sessions to the server.js

--- server.js
+++ server.js
@@ -7,11 +7,39 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require('express-session');
 const PORT = process.env.PORT || 3001;
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Sets up session and connect to our Sequelize db
+// Configure and link a session object with the sequelize store
+const sess = {
+    secret: 'Super secret secret',
+    // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
+    cookie: {
+        // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
+        maxAge: 300000,
+        // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+        httpOnly: true,
+        // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+        secure: false,
+        // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    // Sets up session store
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+// Add express-session and store as Express.js middleware
+app.use(session(sess));
 
 // Define routes
 app.use(routes);
