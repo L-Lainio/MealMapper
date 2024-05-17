f1e5a188eefd7a9b6d0d9360d619837c9bbaf39a
iturnberg <ian.r.turnberg@gmail.com>
2024-05-17 11:52:19 -0400

    im gunna fight the modals

--- public/js/modal2.js
+++ public/js/modal2.js
@@ -1,114 +1,86 @@
+// Function to open modal and clear its content
+function openModal(modal) {
+    modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(modal) {
+    modal.style.display = 'none';
+    // clearModalContent(modal);
+}
+
+// Function to clear modal content
+function clearModalContent(modal) {
+    // Find the modal content element
+    const modalContent = modal.querySelector('.modal-content');
+    // Clear its inner HTML
+    modalContent.innerHTML = '';
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to open modal
-    function openModal(modal) {
-        modal.style.display = 'block';
-    }
-
-    // Function to close modal
-    function closeModal(modal) {
-        modal.style.display = 'none';
-        clearModalContent(modal); // Clear content of the modal when closing
-    }
-
-    // Function to clear modal content
-    function clearModalContent(modal) {
-        if (modal.id === 'ingredient-search-modal') {
-            clearSearchResults();
-            clearFoodDetails();
-            clearAmountInput();
-            clearNutrientResults();
-            clearSearchInput();
-        }
-        // Add similar clearing functions for other modals if needed
-    }
-
-    function clearSearchInput() {
-        const searchInput = document.getElementById('query');
-        if (searchInput) {
-            searchInput.value = ''; // Clear search input value
-        }
-    }
-
-    // Dummy functions for clearing modal content
-    function clearSearchResults() {
-        const searchResults = document.getElementById('ingredient-results');
-        if (searchResults) searchResults.innerHTML = '';
-    }
-
-    function clearFoodDetails() {
-        const foodDetails = document.getElementById('ingredient-food-details');
-        if (foodDetails) foodDetails.innerHTML = '';
-    }
-
-    function clearAmountInput() {
-        const amountInput = document.getElementById('serving-size');
-        if (amountInput) amountInput.value = '';
-    }
-
-    function clearNutrientResults() {
-        const nutrientResults = document.getElementById('ingredient-nutrient-results');
-        if (nutrientResults) nutrientResults.innerHTML = '';
-    }
-
-    // Get all modals
-    const modals = document.querySelectorAll('.modal');
+    // Get the modals
+    const foodModal = document.getElementById('food-modal');
+    const ingredientModal = document.getElementById('ingredient-search-modal');
+    const datepickerModal = document.getElementById('datepicker-modal');
 
     // Get the buttons that open the modals
-    const openModalButtons = {
-        foodModal: document.querySelectorAll('.recipe-btn'),
-        ingredientSearchModal: document.querySelectorAll('.ingredient-btn'),
-        datepickerModal: [document.getElementById('open-datepicker-btn')],
-    };
-
-    // Attach event listeners to open modal buttons
-    for (const [modalId, buttons] of Object.entries(openModalButtons)) {
-        const modal = document.getElementById(modalId);
-        if (modal) {
-            buttons.forEach(button => {
-                if (button) {
-                    button.addEventListener('click', function () {
-                        openModal(modal);
-                    });
-                }
-            });
-        }
-    }
+    const openFoodModalBtns = document.querySelectorAll('.recipe-btn');
+    const openIngrendientModalBtns = document.querySelectorAll('.ingredient-btn');
+    const openDatepickerModalBtn = document.getElementById('open-datepicker-btn');
 
     // Get the close buttons
-    const closeButtons = document.querySelectorAll('.close, .close-modal');
+    const closeButtons = document.querySelectorAll('.close');
 
-    // Attach event listeners to close buttons
+    // Function to set targetSectionId globally
+    function setTargetSectionId(newId) {
+        targetSectionId = newId;
+    }
+
+    // Event listener for open food modal buttons
+    openFoodModalBtns.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.stopPropagation(); // Stop the event from propagating
+            targetSectionId = this.getAttribute('data-target');
+            setTargetSectionId(this.getAttribute('data-target')); // Update targetSectionId
+            openModal(foodModal);
+        });
+    });
+
+      // Event listener for open food modal buttons
+      openIngrendientModalBtns.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.stopPropagation(); // Stop the event from propagating
+            targetSectionId = this.getAttribute('data-target');
+            setTargetSectionId(this.getAttribute('data-target')); // Update targetSectionId
+            openModal(ingredientModal);
+        });
+    });
+
+    // Event listener for open datepicker modal button
+    openDatepickerModalBtn.addEventListener('click', function () {
+        openModal(datepickerModal);
+    });
+
+    // Event listeners for close buttons
     closeButtons.forEach(button => {
         button.addEventListener('click', function () {
-            const modal = this.closest('.modal');
-            if (modal) {
-                closeModal(modal);
-            }
+            closeModal(this.closest('.modal'));
         });
     });
 
-    // Event listener to close modals when clicking outside the modal content
+    // Event listeners to close modals when clicking outside the modal
     window.addEventListener('click', function (event) {
-        modals.forEach(modal => {
-            if (event.target === modal) {
-                closeModal(modal);
-            }
-        });
-    });
-
-    // Event listener to close modals when pressing the Escape key
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape') {
-            modals.forEach(modal => {
-                closeModal(modal);
-            });
+        if (event.target === foodModal || event.target === datepickerModal) {
+            closeModal(event.target);
         }
     });
 
-    // Prevent modal from closing when clicking inside the modal content
-    document.querySelectorAll('.modal-content').forEach(modalContent => {
-        modalContent.addEventListener('click', function (event) {
-            event.stopPropagation();
-        });
+    // Event listener to prevent modal from closing when clicking inside the modal content
+    foodModal.querySelector('.modal-content').addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent click event from bubbling up to the window
+    });
+
+    datepickerModal.querySelector('.modal-content').addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent click event from bubbling up to the window
     });
 });
